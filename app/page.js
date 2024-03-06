204e8c10bc6eba6a34d6aa32910a3271cd01ba76
Arshad <arsh99188@gmail.com>
2024-03-06 12:35:21 +0530

    tost msg

--- app/page.js
+++ app/page.js
@@ -1,23 +1,55 @@
 "use client";
 import React, { useState } from "react";
 
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
 const page = () => {
   let [title, setTitle] = useState("");
   let [desc, setDesc] = useState("");
   const [tasklist, setTasklist] = useState([]);
   //const[copytask, setCopytask] = useState([])
 
+  const deltost = () => {
+    toast.success("🦄 Task is removed", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
+  const notify = () => {
+    toast("🦄 task is added", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+
   const submitTask = (e) => {
     e.preventDefault();
     setTasklist([...tasklist, { title, desc }]);
     setTitle("");
     setDesc("");
+    notify();
   };
 
   const deleteHandeler = (i) => {
     let copytask = [...tasklist];
     copytask.splice(i, 1);
     setTasklist(copytask);
+    deltost();
   };
 
   const changeValueTitle = (e) => {
@@ -75,6 +107,19 @@ const page = () => {
           </button>
         </div>
       ))}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 };
